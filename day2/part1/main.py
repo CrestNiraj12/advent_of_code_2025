ec4d617a6248085c75480442a38fe65817368645
Niraj Shrestha <crestniraj@gmail.com>
2025-12-19 14:28:10 +0545

    ♻️ refactor(day2): simplify and optimize password validation
    Refactor password validation logic for improved readability and
    performance. Replace list with tuple, unpack range values in loop,
    remove unnecessary validation checks, and simplify variable names
    and conditions.

--- day2/part1/main.py
+++ day2/part1/main.py
@@ -2,21 +2,14 @@ def sum_of_invalid_ids() -> int:
     iid = 0
     with open("../input.txt") as f:
         ranges = [
-            list(map(lambda x: int(x), r.split("-")))
-            for r in f.readline().strip().split(",")
+            tuple(map(int, r.split("-"))) for r in f.readline().strip().split(",")
         ]
-        for r in ranges:
-            if len(r) != 2:
-                continue
-
-            for i in range(r[0], r[1] + 1):
-                str_i = str(i)
-                if len(str_i) % 2 != 0:
-                    continue
-
-                mi = len(str_i) // 2
-                if str_i[:mi] == str_i[mi:]:
-                    iid += i
+        for start, end in ranges:
+            for n in range(start, end + 1):
+                s = str(n)
+                mi = len(s) // 2
+                if len(s) % 2 == 0 and s[:mi] == s[mi:]:
+                    iid += n
 
     return iid
 
