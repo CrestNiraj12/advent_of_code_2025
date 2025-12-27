1e1d725e7d55a964811dab258415989f674f4011
Niraj Shrestha <crestniraj@gmail.com>
2025-12-27 20:10:12 +0545

    ✨ feat(day3): Add solution for maximum joltage calculation
    Implement algorithm to find and sum the two highest digits per line
    from input data to calculate total output joltage

---
+++ day3/main.py
@@ -0,0 +1,28 @@
+def getMaximumJoltage() -> int:
+    with open("./input.txt") as f:
+        total = 0
+        for line in f:
+            line = line.strip()
+            line_len = len(line)
+            if line_len <= 2:
+                total += int(line)
+                continue
+
+            ai = 0
+            best_a, best_b = line[0], line[1]
+            for i in range(1, line_len):
+                sn = line[i]
+                n = int(sn)
+                if i < line_len - 1 and n > int(best_a):
+                    best_a = sn
+                    ai = i
+                    best_b = line[i + 1]
+                elif n > int(best_b) and i > ai:
+                    best_b = sn
+            total += int(best_a + best_b)
+        return total
+
+
+if __name__ == "__main__":
+    toj = getMaximumJoltage()
+    print("The total output joltage is:", toj)
